ba0ab1f52604e975b13a7bdca4698e6244d665ed
Jye Bussa <bussajye@gmail.com>
2019-09-04 15:46:43 +1000

    ship class/types setup

---
+++ lib/ships.rb
@@ -0,0 +1,21 @@
+class ships 
+  def initialize(pos, size)
+    @pos = pos 
+    @size = size
+  end
+
+  def carrier
+  end
+
+  def battleship
+  end
+
+  def cruiser 
+  end
+
+  def submarine 
+  end
+
+  def destroyer 
+  end
+end
